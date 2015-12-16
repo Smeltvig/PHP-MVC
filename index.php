acba67e5d683fd87bdf853acdbb1cb6cefa44e39
Smeltvig <thomas@smeltvig.dk>
2015-12-16 16:47:53 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'libs/Val.php';
+require 'libs/Bootstrap.php';
+require 'libs/View.php';
+require 'libs/Controller.php';
+require 'libs/Model.php';
+
+//require 'libs/ObjectDTO.php';
+//require 'libs/Status.php';
+//require 'libs/Logon.php';
+//require 'libs/DB.php';
+$app = new bootstrap();
+?>
