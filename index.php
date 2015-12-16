448bd0c4574aeedfbd0cafb8c119a65acc034f92
Smeltvig <thomas@smeltvig.dk>
2015-12-16 16:51:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-require 'libs/Val.php';
 require 'libs/Bootstrap.php';
 require 'libs/View.php';
 require 'libs/Controller.php';
 require 'libs/Model.php';
-
-//require 'libs/ObjectDTO.php';
-//require 'libs/Status.php';
-//require 'libs/Logon.php';
-//require 'libs/DB.php';
+require 'libs/ObjectDTO.php';
+require 'libs/Status.php';
+require 'libs/Logon.php';
+require 'libs/DB.php';
 $app = new bootstrap();
 ?>
